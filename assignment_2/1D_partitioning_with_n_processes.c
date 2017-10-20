ca22b6f49747c6e21e7e7b658a39cbfe6392d229
Riyansh Karani <riyansh.karani.011235@gmail.com>
2017-10-20 06:27:58 -0500

    implemented Gaussian elimination

--- assignment_2/1D_partitioning_with_n_processes.c
+++ assignment_2/1D_partitioning_with_n_processes.c
@@ -5,11 +5,14 @@
 
 #define SOURCE_NODE                     0
 #define N                               4
-#define MAX_ARRAY_ELEMENT               4
+#define MAX_ARRAY_ELEMENT               20
 
 int LU_Decomposition();
 int randInt();
+void send(int, int *, int, int);
+void receive(int, int *, int, int);
 void printMatrix(int *, int);
+void printArray(int *, int);
 void log_output(char *);
 
 int main(int argc, char **argv) {
@@ -38,24 +41,71 @@ int LU_Decomposition() {
     int id;
     MPI_Comm_rank(MPI_COMM_WORLD, &id);			// get process rank number
     
-    int * ptr;
-    ptr = malloc(pow(N,2) * sizeof(int));
+    int * row;
+    int * temprow;
+    row = malloc(N * sizeof(int));
+    temprow = malloc(N * sizeof(int));
 
     if(id == SOURCE_NODE) {
-        // source node generates the array
+        // source node generates the NxN matrix
+        int * ptr;
+        ptr = malloc(pow(N,2) * sizeof(int));
+
+        // seed the random number generator so that it generates
+	    // different sequence of random numbers after each execution
+	    int seed = time(NULL);
+	    srand(seed);
+
+        // fill the matrix randomly
         for(int i=0; i<pow(N,2); i++) {
             *(ptr+i) = randInt();
         }
+
         printMatrix(ptr, N);
+
+        // brodcast each row of the array to the corresponding processor
+        for(int k=0; k<N; k++) {
+            if(k != SOURCE_NODE) {
+                send(SOURCE_NODE, ptr + (k*N), N, k);
+            }
+        }
+
+        // store the one row used by the source processor
+        memcpy(row, ptr + (N*SOURCE_NODE), N * sizeof(int));
+
+        // free the matrix since it will no longer be used
+        // only the rows will be used to compute the LU Decomposition
+        free(ptr);
+    } else {
+        // receive the row corresponding to this processor,
+        // from the source node
+        receive(id, row, N, SOURCE_NODE);
     }
 
-    // brodcast each row of the array to the corresponding processor
-    for(int k=0; k<N; k++) {
+    printArray(row, N);
 
+    // kth iteration of LU Decomposition
+    for(int k=0; k<N; k++) {
+        if(id == k) {
+            for(int i=k+1; i<N; i++) {
+                send(id, row, N, i);
+            }
+        } else if(id > k){
+            receive(id, temprow, N, k);
+            double pivot = (1.0 * (*(row + k))) / (*(temprow + k));
+
+            for(int j=k; j<N; j++) {
+                *(row + j) -= (*(temprow + j)) * pivot;
+            }
+        }
+
+        printf("process %d for k = %d\n", id, k);
+        printArray(row, N);
     }
 
     // cleanup
-    free(ptr);
+    free(row);
+    free(temprow);
     MPI_Finalize();
 }
 
@@ -63,7 +113,48 @@ int LU_Decomposition() {
  * Generates a random integer in the range [-max, +max]
  */
 int randInt() {
-	return (rand() % (MAX_ARRAY_ELEMENT+1));
+    return (rand() % (MAX_ARRAY_ELEMENT+1)) * pow(-1, (rand() % 2));
+}
+
+/*
+MPI_Send(
+    void* data,
+    int count,
+    MPI_Datatype datatype,
+    int destination,
+    int tag,
+	MPI_Comm communicator)
+*/
+void send(int id, int *ptr, int size, int destination_process_number) {
+	MPI_Send(ptr, size, MPI_INT, destination_process_number, 1, MPI_COMM_WORLD);
+	char s[64];
+	snprintf(s, sizeof(s), "process %d sent %d integers to process: %d\n", id, size, destination_process_number);
+	log_output(s);
+}
+
+/*
+MPI_Recv(
+    void* data,
+    int count,
+    MPI_Datatype datatype,
+    int source,
+    int tag,
+    MPI_Comm communicator,
+    MPI_Status* status)
+ */
+void receive(int id, int *ptr, int size, int source_process_number) {
+	MPI_Recv(ptr, size, MPI_INT, source_process_number, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	char s[64];
+	snprintf(s, sizeof(s), "process %d received %d integers from process: %d\n", id, size, source_process_number);
+	log_output(s);
+}
+
+void printArray(int *ptr, int size) {
+	printf("[");
+	for(int i=0; i<size; i++) {
+		printf("%d, ", *(ptr + i));
+	}
+	printf("]\n");
 }
 
 /*
