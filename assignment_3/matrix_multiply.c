6b294dc5f55ed3e495ae19a3011671113ee21131
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-11-14 11:50:41 -0600

    assignment3

--- assignment_3/matrix_multiply.c
+++ assignment_3/matrix_multiply.c
@@ -10,10 +10,11 @@
 void fillMatrixINputMethod1(int *, int, int);
 void fillMatrixInputMethod2(double * , int);
 void matrixMultiplyKTimes();
+void multiplyMatrices(MPI_Comm, double *, double *, int) ;
 void matrixMultiply();
 int randInt();
-void send(int, int *, int, int);
-void receive(int, int *, int, int);
+void send(int, double *, int, int);
+void receive(int, double *, int, int);
 void printMatrix(double *, int);
 void printArray(double *, int);
 void logOutput(char *);
@@ -28,7 +29,7 @@ int main(int argc, char **argv) {
     
     MPI_Init(NULL, NULL); 									// Initialize the MPI environment
     MPI_Comm_size(MPI_COMM_WORLD, &world_size); 			// get total number of processes
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);						// get process rank number
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);					// get process rank number
     MPI_Get_processor_name(processor_name, &name_len); 		// get the processor name
     
     gethostname(hostname, 255);								// non-MPI function to get the host name
@@ -57,27 +58,84 @@ void matrixMultiplyKTimes() {
     char s[128];
     snprintf(s, sizeof(s), "process : %d has coordinates (%d, %d)\n", rank, coordinates[0], coordinates[1]);
     logOutput(s);   
-    int id;
 
-    MPI_Cart_rank(Cart, (int[2]){0, 1}, &id);
-
-    double * A = malloc(sizeof(double) * pow(N / sqrt(p), 2));
-    double * B = malloc(sizeof(double) * pow(N / sqrt(p), 2));
-
+    int * X = malloc(sizeof(double) * N * N);
     if(rank == SOURCE_NODE) {
-        // generate the source matrix
-        double * X;
-        X = malloc(sizeof(double) * N * N);
+        // generate the source matrix X
         fillMatrixInputMethod2(X, N);
-        free(X);
-    } else {
-
     }
+
+    multiplyMatrices(Cart, X, X, N);
 
 }
 
-void multiplyMatrices(double * X, double * Y, int n) {
-    
+void multiplyMatrices(MPI_Comm Cart, double * X, double * Y, int n) {
+    int coordinates[2];
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Cart_coords(Cart, rank, 2, coordinates);
+
+    int sqrtp = sqrt(p);
+    int blockSize = n / sqrtp;
+    double * A = malloc(sizeof(double) * pow(blockSize, 2));
+    double * B = malloc(sizeof(double) * pow(blockSize, 2));
+
+    // SEND BLOCK DATA TO CORRESPONDING MESSAGES
+    for(int i=0; i<sqrtp; i++) {
+        for(int j=0; j<sqrtp; j++) {
+            printf("(%d, %d)\n", i, j);
+            printf("(-%d, -%d\n", coordinates[i], coordinates[j]);
+            // send part of X to processor (i, j)
+            if(rank == SOURCE_NODE) {
+                int destinationId;
+                MPI_Cart_rank(Cart, (int[2]){i, j}, &destinationId);
+
+                if(destinationId != SOURCE_NODE) {
+                    // send data to this destination id
+                    for(int k=0; k<blockSize; k++) {
+                        send(rank, X + (n * (i+k)) + j, blockSize, destinationId);
+                    }
+                    for(int k=0; k<blockSize; k++) {
+                        send(rank, Y + (n * (i+k)) + j, blockSize, destinationId);
+                    }
+                } else {
+                    // copy data to A and B
+                    for(int k=0; k<blockSize; k++) {
+                        memcpy(A + (k*blockSize), X + (n * (i+k)) + j, sizeof(double) * blockSize);
+                    }
+                    for(int k=0; k<blockSize; k++) {
+                        memcpy(B + (k*blockSize), Y + (n * (i+k)) + j, sizeof(double) * blockSize);
+                    }
+                }
+            } else if(i == coordinates[0]  && j == coordinates[1]) {
+                // receive data for A and B from source
+                for(int k=0; k<blockSize; k++) {
+                    receive(rank, A+(k*blockSize), blockSize, SOURCE_NODE);
+                }
+                for(int k=0; k<blockSize; k++) {
+                    receive(rank, B+(k*blockSize), blockSize, SOURCE_NODE);
+                }
+                memcpy(B, A, sizeof(A));
+            }
+        }
+    }
+
+    printf("process %d has the following data \n", rank);
+    printMatrix(A, blockSize);
+    printMatrix(B, blockSize);
+
+    free(X);
+}
+
+void simpleMultiplyMatrices(double * A, double * B, double * C, int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            *(C + (n*i) + j) = 0;
+            for(int k=0; k<n; k++) {
+                *(C + (n*i) + j) = *(A + (n*i) + k) * *(B + (n*k) + j);
+            }
+        }
+    }
 }
 
 void fillMatrixINputMethod1(int *ptr, int length, int width) {
@@ -121,8 +179,8 @@ MPI_Send(
     int tag,
 	MPI_Comm communicator)
 */
-void send(int id, int *ptr, int size, int destination_process_number) {
-	MPI_Send(ptr, size, MPI_INT, destination_process_number, 1, MPI_COMM_WORLD);
+void send(int id, double *ptr, int size, int destination_process_number) {
+	MPI_Send(ptr, size, MPI_DOUBLE, destination_process_number, 1, MPI_COMM_WORLD);
 	char s[64];
 	snprintf(s, sizeof(s), "process %d sent %d integers to process: %d\n", id, size, destination_process_number);
 	logOutput(s);
@@ -138,8 +196,8 @@ MPI_Recv(
     MPI_Comm communicator,
     MPI_Status* status)
  */
-void receive(int id, int *ptr, int size, int source_process_number) {
-	MPI_Recv(ptr, size, MPI_INT, source_process_number, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+void receive(int id, double *ptr, int size, int source_process_number) {
+	MPI_Recv(ptr, size, MPI_DOUBLE, source_process_number, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	char s[64];
 	snprintf(s, sizeof(s), "process %d received %d integers from process: %d\n", id, size, source_process_number);
 	logOutput(s);
@@ -158,11 +216,12 @@ void printMatrix(double *ptr, int n) {
     snprintf(s, sizeof(s), "printing a matrix of size %d x %d\n", n, n);
     logOutput(s);
     for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            char s[512];
-            snprintf(s, sizeof(s), "%lf ", *(ptr + (n*i) + j));
-            logOutput(s);
-        }
+        // for(int j=0; j<n; j++) {
+        //     char s[512];
+        //     snprintf(s, sizeof(s), "%lf ", *(ptr + (n*i) + j));
+        //     logOutput(s);
+        // }
+        printArray(ptr + (i*n), n);
         logOutput("\n");
     }
 }
