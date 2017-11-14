c803602ea984a438595e5825767fadca190d8300
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-11-14 08:15:26 -0600

    assignment3

--- assignment_3/matrix_multiply.c
+++ assignment_3/matrix_multiply.c
@@ -69,8 +69,9 @@ void matrixMultiplyKTimes() {
         double * X;
         X = malloc(sizeof(double) * N * N);
         fillMatrixInputMethod2(X, N);
-        printMatrix(X, N);
-        // free(X);
+        // printMatrix(X, N);
+        printArray(X, N*N);
+        free(X);
     } else {
 
     }
